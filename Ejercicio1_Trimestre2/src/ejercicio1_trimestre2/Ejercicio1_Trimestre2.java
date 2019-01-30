1aa42bd46fb596cff5e892f73de8cd35b51fa22d
Sergio García <sergio.garcia@cesjuanpablosegundo.es>
2019-01-30 11:45:02 +0100

    Versión 2 (Sin control de errores)

--- Ejercicio1_Trimestre2/src/ejercicio1_trimestre2/Ejercicio1_Trimestre2.java
+++ Ejercicio1_Trimestre2/src/ejercicio1_trimestre2/Ejercicio1_Trimestre2.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio1_trimestre2;
 
-/**
- *
- * @author Usuario DAM 1
- */
+import java.util.Scanner;
+
 public class Ejercicio1_Trimestre2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce la nota del primer examen:");
+        double i = sc.nextDouble();
+        System.out.println("¿Qué nota quieres sacar en el trimestre?");
+        double j = sc.nextDouble();
+        double a = j - (i * 0.4);
+        double nota = a / 0.6;
+        System.out.println("Para tener un "+ i +" en el trimestre necesitas sacar un "+ nota +" en el segundo examen");
     }
-    
+
 }
